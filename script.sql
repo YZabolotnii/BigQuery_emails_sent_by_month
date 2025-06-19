971e8f9c14f990e18cdaa7097233d61808e124f4
Yaroslav <131529206+YZabolotnii@users.noreply.github.com>
2025-06-19 19:38:38 +0300

    Update script.sql

--- script.sql
+++ script.sql
@@ -10,12 +10,12 @@ WITH date_and_mess_count AS (
     , count(es.id_message) over(partition by es.id_account, date_trunc(date_add(s.date, interval es.sent_date DAY), MONTH)) as message_cnt_month
     , min(date_add(s.date, interval es.sent_date DAY)) over(partition by es.id_account) as first_sent_date
     , max(date_add(s.date, interval es.sent_date DAY)) over(partition by es.id_account) as last_sent_date
-  FROM `data-analytics-mate.DA.email_sent` es
-  JOIN `data-analytics-mate.DA.account` ac
+  FROM `email_sent` es
+  JOIN `account` ac
   ON ac.id = es.id_account
-  JOIN `data-analytics-mate.DA.account_session` acs
+  JOIN `account_session` acs
   ON ac.id = acs.account_id
-  JOIN `data-analytics-mate.DA.session` s
+  JOIN `session` s
   ON s.ga_session_id = acs.ga_session_id
 )
 SELECT
